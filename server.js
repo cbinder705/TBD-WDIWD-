62625ca531faa4ddfcb8ee53da4181c488c7fe00
Cory Bee <cbinder705@gmail.com>
2022-12-01 17:02:27 -0500

    implemented format date helper

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-//const helpers = require("./utils/helpers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
