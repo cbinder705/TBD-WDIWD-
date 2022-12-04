3806735c53a0b51c316ddb18d271dccd6bedc472
Cory Bee <cbinder705@gmail.com>
2022-12-04 18:11:30 -0500

    fixed route with event list button

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace("/event/events");
   } else {
     alert(response.statusText);
   }
---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#event-name").value.trim();
+  const elocation = document.querySelector("#event-location").value.trim();
+  const description = document.querySelector("#event-desc").value.trim();
+
+  if (name && elocation && description) {
+    const response = await fetch(`/api/event`, {
+      method: "POST",
+      body: JSON.stringify({ name, description, elocation }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create project");
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/event/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to remove event");
+    }
+  }
+};
+
+document
+  .querySelector(".new-event-form")
+  .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".event-list")
+  .addEventListener("click", delButtonHandler);
