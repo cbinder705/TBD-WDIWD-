ff8c96794d5c55da4bc5ed96d8d6da140a1a8a7f
Cory Bee <cbinder705@gmail.com>
2022-12-05 17:04:22 -0500

    added changes to readme

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  // common addon for heroku containers - jawsdb just like mariadb - mysql
+  sequelize = new Sequelize(process.env.JAWSDB_URL); // connection string  jawsdb.com/^87sda8s7d6f86a87sd6f)
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
